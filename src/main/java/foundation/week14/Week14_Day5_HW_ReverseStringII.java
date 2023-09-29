bba0a3f2d448847bfbe2d89fdf3b4683de1bd42d
Vijoy Vijayan <vijoylearning@gmail.com>
2023-09-29 19:35:09 +0530

    Completed Leetcode problem : Reverse String 2

---
+++ src/main/java/foundation/week14/Week14_Day5_HW_ReverseStringII.java
@@ -0,0 +1,92 @@
+package foundation.week14;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Week14_Day5_HW_ReverseStringII {
+
+	/* 
+Leetcode link: https://leetcode.com/problems/find-all-anagrams-in-a-string/description/
+
+
+	Input 			----->  String, int
+	Output 			----->  String
+
+	Data Set:
+		 Positive: 	---> Input: "orange",2			        Output =  "roaneg"
+		 Negative: 	---> Input: "pondy", 3                  Output =   "nopyd"
+		 Edge: 	    ---> Input: "silent",6		            Output=   "tnelis"
+		 Edge: 	    ---> Input: "a",1		                Output=   "a"
+
+    Algorithm: Sliding Window - Static
+
+	Pseudo code:
+    1. Get the input string and k and initialise the boolean flag as true, StringBuilder result
+    2. Iterate through a for loop with ptr as 0 iterating through ptr < length and incrementing ptr with k
+    3. Get the substring of by passing ptr as start and ptr+k as end
+    4. If flag is true reverse the substring and append it to result
+    5. Reverse the flag and reset the substring to empty
+    6. Check for the terminating condition
+    7. Return the result
+
+	 */
+
+    @Test
+    public void allTests() {
+
+        Assert.assertEquals("roaneg",reverseString2("orange", 2));
+        Assert.assertEquals("nopyd",reverseString2("pondy", 3 ));
+        Assert.assertEquals("tnelis",reverseString2("silent",6	));
+        Assert.assertEquals("a",reverseString2("a",1));
+        Assert.assertEquals("roaneg",reverseString2("orange", 2));
+
+
+        /*
+
+             Positive: 	---> Input: "orange",2			        Output =  "roaneg"
+             Negative: 	---> Input: "pondy", 3                  Output =   "nopyd"
+             Edge: 	    ---> Input: "silent",6		            Output=   "tnelis"
+             Edge: 	    ---> Input: "a",1		                Output=   "a"
+
+         */
+
+
+    }
+
+    public String reverseString2(String s, int k) {
+
+        StringBuilder result = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        int ptr=0;
+        boolean flag = true;
+
+        for (; ptr < s.length() - k; ptr = ptr + k) {
+
+            temp.append(s.substring(ptr, ptr + k));
+
+            if (flag)
+                temp.reverse();
+
+            result.append(temp);
+
+            flag = !flag;
+            temp.setLength(0);
+
+        }
+        temp.append(s.substring(ptr, s.length()));
+
+        if (flag)
+            temp.reverse();
+
+        return result.append(temp).toString();
+
+    }
+
+}
+
+
