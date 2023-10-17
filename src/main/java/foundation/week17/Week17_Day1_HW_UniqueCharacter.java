6265233e6eb807ae9cd9885b96f9aefa705d4bcf
Vijoy Vijayan <vijoylearning@gmail.com>
2023-10-17 10:05:03 +0530

    Solved Leetcode problem: First Unique Character

--- src/main/java/foundation/week17/Week17_Day1_HW_UniqueCharacter.java
+++ src/main/java/foundation/week17/Week17_Day1_HW_UniqueCharacter.java
@@ -20,16 +20,16 @@ Leetcode link: https://leetcode.com/problems/first-unique-character-in-a-string/
 		 Edge: 	    ---> Input: "applealz"		    Output =    7
 
 
-	Pseudo Code: Brute Force
-	1. Get the input and initialise flag to false
-	2. Compare characters in nested loop .. Break each loop if the character is found
-	3. Set flag as true if the unique character is found
-	4. Return indiex if flag is true else return -1
+	Pseudo Code: Frequency Array
+	1. Get the input string and initiate the alphabet array with int type to track all characters
+    2. Iterate through each character of the string and update the value of alphabet array with frequency
+    3. Iterate again within the same string checking for count 1 i.e unique character
+    4. Return the index
 
 
-Best Solution : https://leetcode.com/problems/number-of-equivalent-domino-pairs/solutions/340022/java-c-python-easy-and-concise/
+Best Solution : https://leetcode.com/problems/first-unique-character-in-a-string/solutions/2431836/java-100-faster-code-easy-solution-with-explanation/
 
-Learning: Could have solved using HashMap
+Learning: No need to loop the string multiple times and can be solved with single iteration itself.. Get stronger with the methods of String Library
 
 
 	 */
@@ -37,10 +37,11 @@ Learning: Could have solved using HashMap
     @Test
     public void allTests() {
 
-//        Assert.assertEquals(3,getfirstUniqueCharacter("vijoyvijayan"));
+        Assert.assertEquals(3,getfirstUniqueCharacter("vijoyvijayan"));
         Assert.assertEquals(-1,getfirstUniqueCharacter("abcddcba"));
-//        Assert.assertEquals(0,getfirstUniqueCharacter("x"));
-//        Assert.assertEquals(7,getfirstUniqueCharacter("applealz"));
+        Assert.assertEquals(0,getfirstUniqueCharacter("x"));
+        Assert.assertEquals(4,getfirstUniqueCharacter("applealz"));
+        Assert.assertEquals(10,getfirstUniqueCharacter("mangomangoz"));
 
         /*
          Positive: 	---> Input: "vijoyvijayan"		Output =    3
@@ -53,18 +54,38 @@ Learning: Could have solved using HashMap
     }
 
     public int getfirstUniqueCharacter(String s) {
-        boolean flag=false;
-        int i;
 
-        s.
-        for (i = 0; i < s.length()-1; i++) {
-            for (int j = i+1; j < s.length(); j++) {
-                if(s.charAt(i)==s.charAt(j)) break;
-                if(j==s.length()-1)flag=true;
-            }
-            if(flag)break;
+        int[] alphabetArray=new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            alphabetArray[s.charAt(i)-'a']++;
         }
-        return flag?i:-1;
+
+        for (int i = 0; i < s.length(); i++) {
+            if(alphabetArray[s.charAt(i)-'a']==1)
+                return i;
+        }
+
+        return -1;
+    }
+
+    public int getfirstUniqueCharacter_bestCode(String s) {
+
+        int ans = Integer.MAX_VALUE;
+        // Iterate from a to z which is 26 which makes it constant
+        for(char c='a'; c<='z';c++){
+            // indexOf will return first index of alphabet and lastIndexOf will return last index
+            // if both are equal then it has occured only once.
+            // through this we will get all index's which are occured once
+            // but our answer is lowest index
+            int index = s.indexOf(c);
+            if(index!=-1&&index==s.lastIndexOf(c)){
+                ans = Math.min(ans,index);
+            }
+        }
+
+        // If ans remain's Integer.MAX_VALUE then their is no unique character
+        return ans==Integer.MAX_VALUE?-1:ans;
     }
 }
 
